05d3189cd2863a48801aab69e23ebd7bd92a297d
Rik Lamers <info@riklamers.com>
2018-04-04 21:48:36 +0200

    Added all base scss + grid system

--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,10 @@ module.exports = {
                         }
                     }
                 ]
+            },
+            {
+                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+                loader: 'url-loader?limit=100000'
             }
         ]
     },
