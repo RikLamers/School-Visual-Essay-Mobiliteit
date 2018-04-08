fee78217ccf2a8d649fb3ed03791efa7bd158d1d
Rik Lamers <info@riklamers.com>
2018-04-08 17:55:12 +0200

    Added folder for js structure data

---
+++ src/data/js/gemiddeld_aantal_km_per_inwoner.js
@@ -0,0 +1,75 @@
+export const averageKM = {
+  name: 'linegraph-average',
+  data: [
+    {
+      "Provincie": "Limburg",
+      "Gemiddelde": 10867,
+      "Jaar": 2010
+    },
+    {
+      "Provincie": "Limburg",
+      "Gemiddelde": 10867,
+      "Jaar": 2011
+    },
+    {
+      "Provincie": "Limburg",
+      "Gemiddelde": 10151,
+      "Jaar": 2012
+    },
+    {
+      "Provincie": "Limburg",
+      "Gemiddelde": 11320,
+      "Jaar": 2013
+    },
+    {
+      "Provincie": "Limburg",
+      "Gemiddelde": 10446,
+      "Jaar": 2014
+    },
+    {
+      "Provincie": "Limburg",
+      "Gemiddelde": 10446,
+      "Jaar": 2015
+    },
+    {
+      "Provincie": "Limburg",
+      "Gemiddelde": 10302,
+      "Jaar": 2016
+    },
+    {
+      "Provincie": "Nederland",
+      "Gemiddelde": 12290,
+      "Jaar": 2010
+    },
+    {
+      "Provincie": "Nederland",
+      "Gemiddelde": 12269,
+      "Jaar": 2011
+    },
+    {
+      "Provincie": "Nederland",
+      "Gemiddelde": 12016,
+      "Jaar": 2012
+    },
+    {
+      "Provincie": "Nederland",
+      "Gemiddelde": 12349,
+      "Jaar": 2013
+    },
+    {
+      "Provincie": "Nederland",
+      "Gemiddelde": 12364,
+      "Jaar": 2014
+    },
+    {
+      "Provincie": "Nederland",
+      "Gemiddelde": 11854,
+      "Jaar": 2015
+    },
+    {
+      "Provincie": "Nederland",
+      "Gemiddelde": 11869,
+      "Jaar": 2016
+    }
+  ]
+};
