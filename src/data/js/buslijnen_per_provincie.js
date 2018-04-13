1a25d31a57eda3214a798883118431d42df25206
Rik Lamers <info@riklamers.com>
2018-04-13 14:24:27 +0200

    Added buslijnen js

---
+++ src/data/js/buslijnen_per_provincie.js
@@ -0,0 +1,54 @@
+export const averageBus = {
+  name: 'choropleth',
+  data: [
+      {
+        "provincie": "Noord-Brabant",
+        "buslijnen": 274
+      },
+      {
+        "provincie": "Utrecht",
+        "buslijnen": 186
+      },
+      {
+        "provincie": "Zuid-Holland",
+        "buslijnen": 332
+      },
+      {
+        "provincie": "Noord-Holland",
+        "buslijnen": 335
+      },
+      {
+        "provincie": "Drente",
+        "buslijnen": 69
+      },
+      {
+        "provincie": "Friesland",
+        "buslijnen": 396
+      },
+      {
+        "provincie": "Gelderland",
+        "buslijnen": 279
+      },
+      {
+        "provincie": "Groningen",
+        "buslijnen": 106
+      },
+      {
+        "provincie": "Limburg",
+        "buslijnen": 155
+      },
+      {
+        "provincie": "Overijsel",
+        "buslijnen": 142
+      },
+      {
+        "provincie": "Flevoland",
+        "buslijnen": 55
+      },
+      {
+        "provincie": "Zeeland",
+        "buslijnen": 91
+      },
+    ]
+};
+  
