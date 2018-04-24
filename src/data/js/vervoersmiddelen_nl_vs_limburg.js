6f36fbbd5388e8248003a37e21b74b4bea85f5db
Rik Lamers <info@riklamers.com>
2018-04-24 22:45:36 +0200

    Added data vervoersmiddelen js

---
+++ src/data/js/vervoersmiddelen_nl_vs_limburg.js
@@ -0,0 +1,341 @@
+export const transport = {
+    name: 'barchart-inside-out',
+    data : [
+      {
+        "Vervoerswijze": "Bestuurder2010",
+        "Limburg": 5522,
+        "Nederland": 5719,
+        "SVG": "img/bestuurder.svg"
+      },
+      {
+        "Vervoerswijze": "Passagier2010",
+        "Limburg": 2583,
+        "Nederland": 2968,
+        "SVG": "img/passagier.svg"
+      },
+      {
+        "Vervoerswijze": "Trein2010",
+        "Limburg": 802,
+        "Nederland": 869,
+        "SVG": "img/trein.svg"
+      },
+      {
+        "Vervoerswijze": "BusTramMetro2010",
+        "Limburg": 267,
+        "Nederland": 362,
+        "SVG": "img/bus_metro_tram.svg"
+      },
+      {
+        "Vervoerswijze": "BromSnorFiets2010",
+        "Limburg": 89,
+        "Nederland": 72,
+        "SVG": "img/brom_snorfiets.svg"
+      },
+      {
+        "Vervoerswijze": "Fiets2010",
+        "Limburg": 713,
+        "Nederland": 796,
+        "SVG": "img/fiets.svg"
+      },
+      {
+        "Vervoerswijze": "Lopen2010",
+        "Limburg": 356,
+        "Nederland": 362,
+        "SVG": "img/lopen.svg"
+      },
+      {
+        "Vervoerswijze": "Overig2010",
+        "Limburg": 534,
+        "Nederland": 507,
+        "SVG": "img/overig.svg"
+      },
+      {
+        "Vervoerswijze": "Bestuurder2011",
+        "Limburg": 5790,
+        "Nederland": 6052,
+        "SVG": "img/bestuurder.svg"
+      },
+      {
+        "Vervoerswijze": "Passagier2011",
+        "Limburg": 2227,
+        "Nederland": 2666,
+        "SVG": "img/passagier.svg"
+      },
+      {
+        "Vervoerswijze": "Trein2011",
+        "Limburg": 1069,
+        "Nederland": 1009,
+        "SVG": "img/trein.svg"
+      },
+      {
+        "Vervoerswijze": "BusTramMetro2011",
+        "Limburg": 178,
+        "Nederland": 360,
+        "SVG": "img/bus_metro_tram.svg"
+      },
+      {
+        "Vervoerswijze": "BromSnorFiets2011",
+        "Limburg": 89,
+        "Nederland": 72,
+        "SVG": "img/brom_snorfiets.svg"
+      },
+      {
+        "Vervoerswijze": "Fiets2011",
+        "Limburg": 624,
+        "Nederland": 865,
+        "SVG": "img/fiets.svg"
+      },
+      {
+        "Vervoerswijze": "Lopen2011",
+        "Limburg": 356,
+        "Nederland": 360,
+        "SVG": "img/lopen.svg"
+      },
+      {
+        "Vervoerswijze": "Overig2011",
+        "Limburg": 534,
+        "Nederland": 648,
+        "SVG": "img/overig.svg"
+      },
+      {
+        "Vervoerswijze": "Bestuurder2012",
+        "Limburg": 5253,
+        "Nederland": 5810,
+        "SVG": "img/bestuurder.svg"
+      },
+      {
+        "Vervoerswijze": "Passagier2012",
+        "Limburg": 2048,
+        "Nederland": 2582,
+        "SVG": "img/passagier.svg"
+      },
+      {
+        "Vervoerswijze": "Trein2012",
+        "Limburg": 712,
+        "Nederland": 932,
+        "SVG": "img/trein.svg"
+      },
+      {
+        "Vervoerswijze": "BusTramMetro2012",
+        "Limburg": 267,
+        "Nederland": 359,
+        "SVG": "img/bus_metro_tram.svg"
+      },
+      {
+        "Vervoerswijze": "BromSnorFiets2012",
+        "Limburg": 89,
+        "Nederland": 72,
+        "SVG": "img/brom_snorfiets.svg"
+      },
+      {
+        "Vervoerswijze": "Fiets2012",
+        "Limburg": 712,
+        "Nederland": 861,
+        "SVG": "img/fiets.svg"
+      },
+      {
+        "Vervoerswijze": "Lopen2012",
+        "Limburg": 356,
+        "Nederland": 359,
+        "SVG": "img/lopen.svg"
+      },
+      {
+        "Vervoerswijze": "Overig2012",
+        "Limburg": 712,
+        "Nederland": 646,
+        "SVG": "img/overig.svg"
+      },
+      {
+        "Vervoerswijze": "Bestuurder2013",
+        "Limburg": 5972,
+        "Nederland": 5936,
+        "SVG": "img/bestuurder.svg"
+      },
+      {
+        "Vervoerswijze": "Passagier2013",
+        "Limburg": 2941,
+        "Nederland": 2002,
+        "SVG": "img/passagier.svg"
+      },
+      {
+        "Vervoerswijze": "Trein2013",
+        "Limburg": 713,
+        "Nederland": 1073,
+        "SVG": "img/trein.svg"
+      },
+      {
+        "Vervoerswijze": "BusTramMetro2013",
+        "Limburg": 178,
+        "Nederland": 358,
+        "SVG": "img/bus_metro_tram.svg"
+      },
+      {
+        "Vervoerswijze": "BromSnorFiets2013",
+        "Limburg": 0,
+        "Nederland": 72,
+        "SVG": "img/brom_snorfiets.svg"
+      },
+      {
+        "Vervoerswijze": "Fiets2013",
+        "Limburg": 624,
+        "Nederland": 858,
+        "SVG": "img/fiets.svg"
+      },
+      {
+        "Vervoerswijze": "Lopen2013",
+        "Limburg": 357,
+        "Nederland": 358,
+        "SVG": "img/lopen.svg"
+      },
+      {
+        "Vervoerswijze": "Overig2013",
+        "Limburg": 624,
+        "Nederland": 572,
+        "SVG": "img/overig.svg"
+      },
+      {
+        "Vervoerswijze": "Bestuurder2014",
+        "Limburg": 5446,
+        "Nederland": 5918,
+        "SVG": "img/bestuurder.svg"
+      },
+      {
+        "Vervoerswijze": "Passagier2014",
+        "Limburg": 2143,
+        "Nederland": 2710,
+        "SVG": "img/passagier.svg"
+      },
+      {
+        "Vervoerswijze": "Trein2014",
+        "Limburg": 893,
+        "Nederland": 998,
+        "SVG": "img/trein.svg"
+      },
+      {
+        "Vervoerswijze": "BusTramMetro2014",
+        "Limburg": 268,
+        "Nederland": 285,
+        "SVG": "img/bus_metro_tram.svg"
+      },
+      {
+        "Vervoerswijze": "BromSnorFiets2014",
+        "Limburg": 89,
+        "Nederland": 71,
+        "SVG": "img/brom_snorfiets.svg"
+      },
+      {
+        "Vervoerswijze": "Fiets2014",
+        "Limburg": 714,
+        "Nederland": 998,
+        "SVG": "img/fiets.svg"
+      },
+      {
+        "Vervoerswijze": "Lopen2014",
+        "Limburg": 357,
+        "Nederland": 357,
+        "SVG": "img/lopen.svg"
+      },
+      {
+        "Vervoerswijze": "Overig2014",
+        "Limburg": 625,
+        "Nederland": 642,
+        "SVG": "img/overig.svg"
+      },
+      {
+        "Vervoerswijze": "Bestuurder2015",
+        "Limburg": 5546,
+        "Nederland": 5751,
+        "SVG": "img/bestuurder.svg"
+      },
+      {
+        "Vervoerswijze": "Passagier2015",
+        "Limburg": 2147,
+        "Nederland": 2485,
+        "SVG": "img/passagier.svg"
+      },
+      {
+        "Vervoerswijze": "Trein2015",
+        "Limburg": 984,
+        "Nederland": 923,
+        "SVG": "img/trein.svg"
+      },
+      {
+        "Vervoerswijze": "BusTramMetro2015",
+        "Limburg": 179,
+        "Nederland": 355,
+        "SVG": "img/bus_metro_tram.svg"
+      },
+      {
+        "Vervoerswijze": "BromSnorFiets2015",
+        "Limburg": 89,
+        "Nederland": 71,
+        "SVG": "img/brom_snorfiets.svg"
+      },
+      {
+        "Vervoerswijze": "Fiets2015",
+        "Limburg": 626,
+        "Nederland": 923,
+        "SVG": "img/fiets.svg"
+      },
+      {
+        "Vervoerswijze": "Lopen2015",
+        "Limburg": 358,
+        "Nederland": 284,
+        "SVG": "img/lopen.svg"
+      },
+      {
+        "Vervoerswijze": "Overig2015",
+        "Limburg": 626,
+        "Nederland": 568,
+        "SVG": "img/overig.svg"
+      },
+      {
+        "Vervoerswijze": "Bestuurder2016",
+        "Limburg": 5465,
+        "Nederland": 5725,
+        "SVG": "img/bestuurder.svg"
+      },
+      {
+        "Vervoerswijze": "Passagier2016",
+        "Limburg": 1971,
+        "Nederland": 2544,
+        "SVG": "img/passagier.svg"
+      },
+      {
+        "Vervoerswijze": "Trein2016",
+        "Limburg": 1075,
+        "Nederland": 989,
+        "SVG": "img/trein.svg"
+      },
+      {
+        "Vervoerswijze": "BusTramMetro2016",
+        "Limburg": 269,
+        "Nederland": 353,
+        "SVG": "img/bus_metro_tram.svg"
+      },
+      {
+        "Vervoerswijze": "BromSnorFiets2016",
+        "Limburg": 90,
+        "Nederland": 71,
+        "SVG": "img/brom_snorfiets.svg"
+      },
+      {
+        "Vervoerswijze": "Fiets2016",
+        "Limburg": 627,
+        "Nederland": 848,
+        "SVG": "img/fiets.svg"
+      },
+      {
+        "Vervoerswijze": "Lopen2016",
+        "Limburg": 358,
+        "Nederland": 353,
+        "SVG": "img/lopen.svg"
+      },
+      {
+        "Vervoerswijze": "Overig2016",
+        "Limburg": 448,
+        "Nederland": 636,
+        "SVG": "img/overig.svg"
+      }
+    ]
+};
